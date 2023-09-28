a11c4359cf3e4e8592f5639f8cd27785a06843af
Alexey Sachkov <alexey.sachkov@intel.com>
2023-09-28 05:32:59 -0700

    properties fixes

--- sycl/include/sycl/ext/oneapi/experimental/virtual_functions.hpp
+++ sycl/include/sycl/ext/oneapi/experimental/virtual_functions.hpp
@@ -16,15 +16,12 @@ template <typename Set = void>
 inline constexpr indirectly_callable_key::value_t<Set> indirectly_callable;
 
 struct calls_indirectly_key {
-  template <typename SetIds = void>
-  using value_t = property_value<calls_indirectly_key, SetIds>;
-      /* std::conditional_t<sizeof...(SetIds) == 0,
-                         property_value<calls_indirectly_key, void>,
-                         property_value<calls_indirectly_key, SetIds...>>;*/
+  template <typename First = void, typename... SetIds>
+  using value_t = property_value<calls_indirectly_key, First, SetIds...>;
 };
 
-template <typename SetIds>
-inline constexpr calls_indirectly_key::value_t<SetIds> calls_indirectly;
+template <typename First = void, typename... Rest>
+inline constexpr calls_indirectly_key::value_t<First, Rest...> calls_indirectly;
 
 template <> struct is_property_key<indirectly_callable_key> : std::true_type {};
 template <> struct is_property_key<calls_indirectly_key> : std::true_type {};
@@ -56,34 +53,37 @@ struct PropertyMetaInfo<indirectly_callable_key::value_t<Set>> {
 };
 
 
-// FIXME: We should be able to pass several sets into calls_indirectly property
-template <typename Set>
-struct PropertyMetaInfo<calls_indirectly_key::value_t<Set>> {
+template <typename First, typename... Rest>
+struct PropertyMetaInfo<calls_indirectly_key::value_t<First, Rest...>> {
   static constexpr const char *name = "uses-indirectly-callable";
   static constexpr const char *value =
 #ifdef __SYCL_DEVICE_ONLY__
-      __builtin_sycl_unique_stable_name(Set);
+      // FIXME: we should handle Rest... here as well
+      __builtin_sycl_unique_stable_name(First);
 #else
       "";
 #endif
 };
-} // namespace detail
-
-} // namespace ext::oneapi::experimental
-} // namespace _V1
-} // namespace sycl
 
 template<typename T = void>
 struct void_or_T {
   using type = T;
 };
 
+} // namespace detail
+
+} // namespace ext::oneapi::experimental
+} // namespace _V1
+} // namespace sycl
+
+
 #ifdef __SYCL_DEVICE_ONLY__
 #define SYCL_EXT_ONEAPI_INDIRECTLY_CALLABLE(SetId)                             \
   [[__sycl_detail__::add_ir_attributes_function(                               \
       "indirectly-callable",                                                   \
       __builtin_sycl_unique_stable_name(                                       \
-          void_or_T<SetId>::type))]] __attribute__((sycl_device))
+          sycl::ext::oneapi::experimental::detail::void_or_T<                  \
+              SetId>::type))]] __attribute__((sycl_device))
 #else
 
 #define SYCL_EXT_ONEAPI_INDIRECTLY_CALLABLE(SetId)
